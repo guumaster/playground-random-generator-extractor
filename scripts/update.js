8025f3c50add319193fb194767d20284025b870e
guumaster <guuweb@gmail.com>
2018-10-13 13:38:50 +0200

    add pulp to excluded

--- scripts/update.js
+++ scripts/update.js
@@ -1,17 +1,26 @@
-const { pick } = require('lodash')
-
-const { getDb } = require('../src/db')
-const groupByCategories = require('../src/db/group_by_categories')
+const { sort, uniq } = require('lodash')
+const { getDb, saveCategories } = require('../src/db')
 
 const main = async () => {
   try {
     const db = await getDb()
 
-    await db.content.get('tables')
-      .filter(x => !x.name.match(/pulp/ig))
-      .write()
+    // Filter pulp tables
+    // await db.content.get('tables')
+    //   // .remove(x => x.name.match(/. pulp/ig))
+    //   .sortBy('name')
+    //   .write()
+    //
+    // await saveCategories()
 
-    await saveCategories()
+
+    // add pulp to excluded
+    const pulp = await db.pages.get('all')
+    .filter(x => x.match(/. pulp/ig))
+      .value()
+
+    const newList = db.pages.get('excluded').value().concat(pulp)
+    await db.pages.set('excluded', uniq(newList).sort()).write()
 
     console.log('done')
 
